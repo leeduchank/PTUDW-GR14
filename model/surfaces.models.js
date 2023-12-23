40b1797a0d63da75fa5bccc68c51c6b6ff6704a8
LamDDT <108422896+LamDDT@users.noreply.github.com>
2023-12-24 00:21:54 +0700

    Update surfaces.models.js
    add findAll()

--- model/surfaces.models.js
+++ model/surfaces.models.js
@@ -1,6 +1,10 @@
 import db from'../ultis/db.js';
 
 export default{
+    findAll() {
+        return db('surfaces');
+    }
+,
     findBySpaceId(spaceId) {
         return db('surfaces').where('space', spaceId);
     }
