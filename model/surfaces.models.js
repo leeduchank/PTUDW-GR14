61f74616221d9b5dc8eac7e9161792cc6cccab65
LamDDT <108422896+LamDDT@users.noreply.github.com>
2023-12-25 08:10:48 +0700

    Update surfaces.models.js

--- model/surfaces.models.js
+++ model/surfaces.models.js
@@ -8,4 +8,13 @@ export default{
     findBySpaceId(spaceId) {
         return db('surfaces').where('space', spaceId);
     }
+,
+    findPageBySpaceId(spaceId, limit, offset) {
+        return db('surfaces').where('space', spaceId).limit(limit).offset(offset);
+    }
+,
+    async countBySpaceId(spaceId) {
+        const list = await db('surfaces').where('space', spaceId).count({ amount: 'id_surfaces' });
+        return list[0].amount;
+    }
 }
